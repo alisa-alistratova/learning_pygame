2b17d668946efd62f3e83edcb60551bb63b0c5c0
Alisa Alistratova <26-037@isriga.lv>
2023-05-25 13:49:41 +0300

    intro how games work

---
+++ how_video_games_work.py
@@ -0,0 +1,37 @@
+#How Video Games Work:
+'''
+kinda ike movies and it moves wdue to indiviudual frames
+24 fps usually
+you could record,draw images and use digital tools
+video games work like a movie (animation)
+'''
+#In a movie:
+# - Images are fixed.
+# - No way to input stuff (except pausing and fastforwarding).
+
+#In a video game:
+# - Images are dynamically generated while the game is running.
+# - Constant checking of player input to updtae what frame is being drawn.
+
+#Creating Frames:
+# 1. Checking player input.
+# 2. Use that info to place elements on the screen.
+# 3. Rinse and Repeat.
+
+#What does Pygame do?:
+'''
+1. Pygame helps you draw images (and play sounds).
+2. Checks for player input. (input() stops your code and is thus useless for games).
+3. Other gamedev tools like collisions, creating text, timers, etc.
+4. Nobody uses Pygame for AAA games or Indie Games.
+'''
+'''
+#Game engnes:
+Programmes that help you make games by giving you tools like level editors, 
+physics engines, skeleton animations, etc. to make games.
+--> Unreal, Unity, Godot...
+'''
+#Why learn Pygame?
+'''
+
+'''
