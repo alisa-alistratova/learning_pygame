766696243d8efe9c0e049f926c149146b26c336a
Alisa Alistratova <26-037@isriga.lv>
2023-05-25 15:15:41 +0300

    display_surfaces

--- starting_pygame.py
+++ starting_pygame.py
@@ -5,6 +5,16 @@ pygame.init()
 screen = pygame.display.set_mode((800,400)) #width = 800, height = 400
 pygame.display.set_caption('Whatever')
 clock = pygame.time.Clock() #controls time & framerate
+'''
+#PLAIN COLOR SURFACE
+test_surface = pygame.Surface((200,100))
+test_surface.fill((195,255,236))
+#test_surface2 = pygame.Surface((600,100))
+#test_surface2.fill((255,255,195))
+'''
+#IMAGE SURFACE
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
 
 while True:
     #event loop
@@ -14,6 +24,11 @@ while True:
             exit()
     #draw all our elements
     #update everything
+
+    #screen.blit(test_surface,(300,300)) #blit - block image transfer
+    #screen.blit(test_surface2,(0,0))
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
     pygame.display.update()
     clock.tick(60)
 
@@ -22,3 +37,4 @@ while True:
 #(Regular) surface - things to put on the display surface (can have as many as necessary)
 
 #Plain Color Surface
+
